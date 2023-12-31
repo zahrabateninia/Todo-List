9d42c5cf9d93f186d8e811dfe176b44cb6b25bdf
zahrabateninia <zahrabatenin@gmail.com>
2023-12-31 13:56:50 +0330

    Done with hard coded html, ready to convert to modules

---
+++ dist/main.js
@@ -0,0 +1,13 @@
+// import { format, compareAsc } from 'date-fns'
+
+// document.getElementById('task-form').addEventListener('submit', function(e) {
+//     e.preventDefault();  // Prevent the default form submission
+    
+//     const dueDateInput = document.getElementById('due-date');
+//     const dueDate = new Date(dueDateInput.value);  // Get the date from the input
+
+//     // Example: Format the date in a readable format
+//     console.log("Formatted Date:", format(dueDate, 'MMMM do, yyyy'));
+
+//     // You can perform other date operations as needed here
+// });
