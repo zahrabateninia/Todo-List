e95334f26bf9f096e613f79b75faf911b17c516e
zahrabateninia <zahrabatenin@gmail.com>
2024-01-03 15:49:07 +0330

    Use flex box for tasks container and each new task style

--- src/toDoItem.js
+++ src/toDoItem.js
@@ -9,45 +9,50 @@ export function renderNewTask(){
     tasksArr.forEach(task =>{
         const taskContainer = document.createElement('div');
         taskContainer.classList.add('task-container');
+        const taskStatusTitle = document.createElement('div');
+        taskStatusTitle.classList.add('task-status-title');
+        const taskDetails = document.createElement('div');
+        taskDetails.classList.add('task-details');
 
         // add the circle icon
         const statusIcon = document.createElement('img');
         statusIcon.classList.add('circle-icon', 'task-icon');
         statusIcon.src = "./images/circle_FILL0_wght400_GRAD0_opsz24.svg"
-        taskContainer.appendChild(statusIcon);
+        taskStatusTitle.appendChild(statusIcon);
         // add task title 
         const taskTitle = document.createElement('p');
         taskTitle.classList.add('task-title');
         taskTitle.textContent = task.title;
-        taskContainer.appendChild(taskTitle);
+        taskStatusTitle.appendChild(taskTitle);
         // add schedule
         const schedule = document.createElement('span');
         schedule.classList.add('schedule');
         schedule.textContent = task.schedule;
-        taskContainer.appendChild(schedule);
+        taskDetails.appendChild(schedule);
 
         // add priority
         const priority = document.createElement('span');
         priority.classList.add('priority');
         priority.textContent = task.priority;
-        taskContainer.appendChild(priority);
+        taskDetails.appendChild(priority);
 
         // add other icons
 
         const editIcon = document.createElement('img');
         editIcon.classList.add('edit-icon', 'task-icon');
         editIcon.src = "./images/edit_FILL0_wght400_GRAD0_opsz24.svg";
-        taskContainer.appendChild(editIcon);
+        taskDetails.appendChild(editIcon);
 
 
         const trashIcon = document.createElement('img');
         trashIcon.classList.add('trash-icon', 'task-icon');
         trashIcon.src = "./images/delete_FILL0_wght400_GRAD0_opsz24.svg";
-        taskContainer.appendChild(trashIcon);
+        taskDetails.appendChild(trashIcon);
 
+        taskContainer.appendChild(taskStatusTitle);
+        taskContainer.appendChild(taskDetails);
         allTasksContainer.appendChild(taskContainer);
     
-        
     })
 
 };
