4b41196fe74f30a06555a16aac88e67a6a4691cb
zahrabateninia <zahrabatenin@gmail.com>
2024-01-25 14:34:01 +0330

    feat: integrate Babel with Webpack for ES6+ compatibility
    - Installed babel-loader, @babel/core, and @babel/preset-env
    - Configured Webpack to use babel-loader for JavaScript files
    - Added Babel configuration to package.json

--- webpack.config.js
+++ webpack.config.js
@@ -8,4 +8,20 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
   },
   devtool: "eval-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // A regex that tells Webpack to use this rule for all .js files
+        exclude: /node_modules/, // It's common to exclude the node_modules folder
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
 };
+
+// This module tells Webpack to use babel-loader for JavaScript files. This will allow Babel to transpile your JavaScript code 
